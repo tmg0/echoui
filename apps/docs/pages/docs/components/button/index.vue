b960773fe4d88bf6eb27ca23f8b176e0a97a1aa3
zekunjin <zekun.jin@foxmail.com>
2023-08-28 11:22:54 +0800

    fix: nuxt monorepo err

---
+++ apps/docs/pages/docs/components/button/index.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { Button } from '@echo-ui/vue'
+</script>
+
+<template>
+  <div>
+    <div class="flex gap-2 p-2">
+      <Button color="primary">
+        Disabled
+      </Button>
+    </div>
+
+    <div class="flex gap-2 p-2">
+      <Button color="primary" is-disabled>
+        Disabled
+      </Button>
+    </div>
+
+    <div class="flex gap-2 p-2">
+      <Button size="sm">
+        Small
+      </Button>
+      <Button size="md">
+        Medium
+      </Button>
+      <Button size="lg">
+        Large
+      </Button>
+    </div>
+
+    <div class="flex gap-2 p-2">
+      <Button radius="full">
+        Full
+      </Button>
+      <Button radius="lg">
+        Large
+      </Button>
+      <Button radius="md">
+        Medium
+      </Button>
+      <Button radius="sm">
+        Small
+      </Button>
+      <Button radius="none">
+        None
+      </Button>
+    </div>
+
+    <div class="flex gap-2 p-2">
+      <Button>
+        Default
+      </Button>
+      <Button color="primary">
+        Primary
+      </Button>
+      <Button color="secondary">
+        Secondary
+      </Button>
+      <Button color="success">
+        Success
+      </Button>
+      <Button color="warning">
+        Warning
+      </Button>
+      <Button color="danger">
+        Danger
+      </Button>
+    </div>
+
+    <div class="flex gap-2 p-2">
+      <Button color="primary" variant="solid">
+        Solid
+      </Button>
+      <Button color="primary" variant="faded">
+        Faded
+      </Button>
+      <Button color="primary" variant="bordered">
+        Bordered
+      </Button>
+      <Button color="primary" variant="light">
+        Light
+      </Button>
+      <Button color="primary" variant="flat">
+        Flat
+      </Button>
+      <Button color="primary" variant="ghost">
+        Ghost
+      </Button>
+      <Button color="primary" variant="shadow">
+        Shadow
+      </Button>
+    </div>
+
+    <div class="flex gap-2 p-2">
+      <Button color="primary" is-disabled is-loading>
+        Loading
+      </Button>
+    </div>
+  </div>
+</template>
