13a0c9dcf757478551e87f93d8fd37c9cdf1761e
zekunjin <zekun.jin@foxmail.com>
2023-08-29 17:52:58 +0800

    style: update grid docs layout

--- apps/docs/app.vue
+++ apps/docs/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <Navbar />
-    <NuxtPage />
+    <main class="relative container mx-auto max-w-7xl z-10 px-6 min-h-[calc(100vh_-_64px_-_108px)] mb-12 flex-grow">
+      <NuxtPage />
+    </main>
   </div>
 </template>
