507bbddc1cae5c3372dbcfd4318985d4ec23fc20
zekunjin <zekun.jin@foxmail.com>
2023-08-31 15:42:05 +0800

    fix: child picker err

--- apps/docs/pages/docs/components/tabs/index.vue
+++ apps/docs/pages/docs/components/tabs/index.vue
@@ -2,6 +2,24 @@
 import { Tabs, Tab } from '@echoui/vue'
 
 const selectedKey = ref('photos')
+
+const tabs = [
+  {
+    id: 'photos',
+    label: 'Photos',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  },
+  {
+    id: 'music',
+    label: 'Music',
+    content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+  },
+  {
+    id: 'videos',
+    label: 'Videos',
+    content: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+]
 </script>
 
 <template>
@@ -15,11 +33,11 @@ const selectedKey = ref('photos')
 
     <div>
       <Tabs v-model:selected-key="selectedKey">
-        <Tab key="photos" title="Photos">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <Tab v-for="item in tabs" :key="item.id" :title="item.label">
+          <div class="p-5 bg-content1 rounded-[14px]">
+            {{ item.content }}
+          </div>
         </Tab>
-        <Tab key="music" title="Music" />
-        <Tab key="videos" title="Videos" />
       </Tabs>
     </div>
   </NuxtLayout>
--- packages/utilities/vue-utils/src/children.ts
+++ packages/utilities/vue-utils/src/children.ts
@@ -2,8 +2,19 @@ import type { VNode, RendererNode, RendererElement } from 'vue'
 
 type VueNode = VNode<RendererNode, RendererElement, { [key: string]: any }>
 
+const isSymbol = (value: any): value is symbol => typeof value === 'symbol'
+
 export const pickChildren = (children: VueNode[] | undefined, targetNode: any) => {
+  if (!children) { return [[], undefined] }
+  if (children.length === 1) {
+    const [child] = children
+    if (isSymbol(child.type)) {
+      children = child.children as any
+    }
+  }
+
   if (!children || !children.length) { return [[], undefined] }
+
   const target: VueNode[] = []
   const withoutTargetChildren = children.map((child: any) => {
     if (child.type.name === targetNode.name) {
