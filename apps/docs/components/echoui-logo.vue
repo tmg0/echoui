ee64c772ceade36815e3ecb27848694630ebdb6e
zekunjin <zekun.jin@foxmail.com>
2023-09-05 17:50:16 +0800

    chore: update logo

--- apps/docs/components/echoui-logo.vue
+++ apps/docs/components/echoui-logo.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-defineProps<{
+interface Props {
   size: number
-}>()
+  fill: string
+}
+
+withDefaults(defineProps<Props>(), {
+  fill: '#2ac0dd'
+})
 </script>
 
 <template>
   <div :style="{ width: `${size}px`, height: `${size}px`}" class="bg-[#11181c] dark:bg-[#ecedee] rounded-lg flex items-center justify-center">
-    <div :style="{ width: `${size * 0.6}px`, height: `${size * 0.6}px`}" class="bg-[#2ac0dd] rounded-full" />
+    <svg
+      viewBox="0 0 1024 1024"
+      :fill="fill"
+      :width="size"
+      :height="size"
+    >
+      <path
+        d="M512 853.333333C370.773333 853.333333 256 738.56 256 597.333333 256 426.666667 512 138.666667 512 138.666667 512 138.666667 768 426.666667 768 597.333333 768 738.56 653.226667 853.333333 512 853.333333Z"
+        :stroke="fill"
+      />
+    </svg>
   </div>
 </template>
