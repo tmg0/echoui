bcb368741a1f4f7cf1dc1b3c8e4520d256684668
zekunjin <zekun.jin@foxmail.com>
2023-09-06 15:47:23 +0800

    chore: update card footer demo txt

--- apps/docs/components/marketing/floating-components.vue
+++ apps/docs/components/marketing/floating-components.vue
@@ -84,7 +84,7 @@ const isSelected = computed({
       />
       <CardFooter class="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
         <p class="text-tiny text-white/80">
-          Comming soon
+          Available ...
         </p>
         <Button class="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
           Notify me
