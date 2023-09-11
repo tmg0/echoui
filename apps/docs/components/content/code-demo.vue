a344d2bb63d3c91e0c34fa04ff148e4c920d4944
zekunjin <zekun.jin@foxmail.com>
2023-09-11 15:00:02 +0800

    chore: use shikiji support code highlight

--- apps/docs/components/content/code-demo.vue
+++ apps/docs/components/content/code-demo.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import { Card, CardBody, Tabs, Tab } from '@echoui/vue'
+import { getHighlighter } from 'shikiji'
 import { modules, codes } from '.'
 
 const props = defineProps<{ files: string }>()
-const code = computed(() => codes[props.files])
+const code = ref('')
 
 const DynamicVueLiveDemo = defineAsyncComponent(() => {
   return modules[props.files]?.() as any
 })
+
+const shiki = await getHighlighter({
+  themes: ['vitesse-dark'],
+  langs: ['vue']
+})
+
+code.value = shiki.codeToHtml(codes[props.files], { lang: 'javascript', theme: 'vitesse-dark' })
 </script>
 
 <template>
@@ -24,8 +32,8 @@ const DynamicVueLiveDemo = defineAsyncComponent(() => {
 
     <Tab key="code" title="Code">
       <Card>
-        <CardBody class="whitespace-pre-wrap">
-          {{ code }}
+        <CardBody class="whitespace-pre-wrap bg-[#121212]">
+          <div v-html="code" />
         </CardBody>
       </Card>
     </Tab>
